ccda48c39b88c78f721613d461c1b518d0a1bad6
JSBigelow <165178317+JSBigelow@users.noreply.github.com>
2025-08-06 10:11:07 +0100

    Updates from beta testing

--- src/sampley/__init__.py
+++ src/sampley/__init__.py
@@ -1,5 +1,5 @@
 
-__all__ = ['DataPoints', 'Sections', 'Periods', 'Cells', 'Segments', 'Presences', 'AbsenceLines', 'Absences', 'Samples']
+__all__ = ['DataPoints', 'Sections', 'Periods', 'Cells', 'Segments', 'Presences', 'PresenceZones', 'Absences', 'Samples']
 
-from .classes import DataPoints, Sections, Periods, Cells, Segments, Presences, AbsenceLines, Absences, Samples
+from .classes import DataPoints, Sections, Periods, Cells, Segments, Presences, PresenceZones, Absences, Samples
 
